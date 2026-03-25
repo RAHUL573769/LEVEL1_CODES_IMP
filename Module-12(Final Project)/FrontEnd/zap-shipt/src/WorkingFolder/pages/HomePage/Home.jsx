6c4df61514cfd988ac79b1f7f3156b397cf01ade
RAHUL RUDRA <rahulrudra665@gmail.com>
2026-03-25 10:15:20 +0600

    first commit

---
+++ Module-12(Final Project)/FrontEnd/zap-shipt/src/WorkingFolder/pages/HomePage/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../../Banner/Banner";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is Home</h1>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
