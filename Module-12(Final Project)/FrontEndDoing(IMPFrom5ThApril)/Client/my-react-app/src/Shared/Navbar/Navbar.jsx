8d75691a8ab17056c8523fa3d13ef8fd51382348
RAHUL RUDRA <rahulrudra665@gmail.com>
2026-04-06 10:02:00 +0600

    odule final project

--- Module-12(Final Project)/FrontEndDoing(IMPFrom5ThApril)/Client/my-react-app/src/Shared/Navbar/Navbar.jsx
+++ Module-12(Final Project)/FrontEndDoing(IMPFrom5ThApril)/Client/my-react-app/src/Shared/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Logo from '../Logo/Logo';
 
 const Navbar = () => {
 
@@ -20,7 +21,9 @@ const Navbar = () => {
                             {navItems}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">
+                        <Logo></Logo>
+                    </a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
