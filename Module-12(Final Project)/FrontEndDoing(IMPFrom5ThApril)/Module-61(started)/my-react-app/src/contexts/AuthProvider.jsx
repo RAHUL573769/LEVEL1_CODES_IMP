d5165b079db7997d8b0387816464a2010403c975
Rahul Rudra <rahulrudra665@gmail.com>
2026-04-13 03:14:24 +0600

    changws mad"

--- Module-12(Final Project)/FrontEndDoing(IMPFrom5ThApril)/Module-61(started)/my-react-app/src/contexts/AuthProvider.jsx
+++ Module-12(Final Project)/FrontEndDoing(IMPFrom5ThApril)/Module-61(started)/my-react-app/src/contexts/AuthProvider.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { auth } from './../configuations/Firebase/FiebaseInit';
 
 const AuthProvider = ({ children }) => {
-
+const provider = new GoogleAuthProvider();
     const registerUser = (email,password) => {
         return createUserWithEmailAndPassword(auth,email,password)
     }
-    const authInfo={registerUser}
+
+    const signInWithEmailAndPasswordFunction = (email, password) => {
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth,provider)
+    }   //must return something
+    const authInfo = {
+        signInWithEmailAndPasswordFunction,
+        registerUser, signInWithGoogle, signInWithEmailAndPassword
+    }
     return (
         <AuthContext value={authInfo}>
 {children}
